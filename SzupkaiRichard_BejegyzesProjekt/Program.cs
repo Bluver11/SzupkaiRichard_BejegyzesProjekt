eee04e3aed84ee6be684ce6c63462781a0e4ae66
ricsi <szupkai.ricsi@petrik.hu>
2023-09-26 09:53:31 +0200

    Bejegyzes osztály megalkotása

---
+++ SzupkaiRichard_BejegyzesProjekt/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzupkaiRichard_BejegyzesProjekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
